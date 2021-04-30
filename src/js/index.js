875dbbffd6a7f733de5b2cc545bc794afa28a2d9
J0hns0n-Wang <johnson.wang23@sitechhs.com>
2021-04-30 10:52:10 -0400

    got data

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,17 @@ import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
 const key = "YOURKEYHERE";
+console.log(key);
+
+const query = async function () {
+  try {
+    const response = await fetch(
+      "https://api.jikan.moe/v3/search/anime?q=&order_by=members&sort=desc&page=1"
+    );
+    const data = await response.json();
+    console.log(data.results);
+  } catch (error) {
+    console.log("Wrong");
+  }
+};
+query();
