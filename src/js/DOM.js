58cbe682b0a1b83133f5a2e80091c34d2eb8ad58
VincentYou <vincent.you23@sitechhs.com>
2021-05-23 09:37:03 -0400

    Revert "heh?"
    This reverts commit a52b1660e63124967f10693f0dd26c703e523679.

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -2,8 +2,6 @@ const DOMSelectors = {
   grid: document.querySelector(".anime-grid"),
   loadBtn: document.querySelector("#load-btn"),
   info: document.querySelector("#anime-info"),
-  searchForm: document.getElementById("search-form"),
-  searchArea: document.getElementById("search-area"),
 };
 
 export { DOMSelectors };
